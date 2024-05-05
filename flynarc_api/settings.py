81dafcce564c3b8d5caf02079b19abde899aca5a
Alma Isaksson <isakvent@outlook.com>
2024-05-05 18:45:33 +0000

    change page size to 10

--- flynarc_api/settings.py
+++ flynarc_api/settings.py
@@ -35,7 +35,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':
     'rest_framework.pagination.PageNumberPagination',
     # initially set to 5
-    'PAGE_SIZE': None,
+    'PAGE_SIZE': 10,
     'DATETIME_FORMAT': '%d %b %Y',
 }
 

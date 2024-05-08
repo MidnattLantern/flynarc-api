74ea0bcd6e22aac85c986bd7f2c0883cba7b1fda
Alma <isakvent@outlook.com>
2024-05-08 10:24:39 +0200

    set pagination to 5 again

--- flynarc_api/settings.py
+++ flynarc_api/settings.py
@@ -32,10 +32,10 @@ REST_FRAMEWORK = {
         if 'DEV' in os.environ
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
     )],
-    'DEFAULT_PAGINATION_CLASS': None,
-#    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': None,
-#    'PAGE_SIZE': 5,
+#    'DEFAULT_PAGINATION_CLASS': None,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+#    'PAGE_SIZE': None,
+    'PAGE_SIZE': 5,
     'DATETIME_FORMAT': '%d %b %Y',
 }
 
@@ -67,6 +67,7 @@ ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
     'localhost',
     'flynarc-api-824d94b4a80f.herokuapp.com',
+    '127.0.0.1',
     ]
 
 
